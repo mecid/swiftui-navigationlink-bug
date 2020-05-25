00a32a2b85b2ce6c6feccf3fea82e976c7507ed5
Majid Jabrayilov <cmecid@gmail.com>
2020-05-26 02:53:51 +0400

    Navigation link bug reproduced

--- NavigationLinkBug/ContentView.swift
+++ NavigationLinkBug/ContentView.swift
@@ -5,12 +5,37 @@
 //  Created by Majid Jabrayilov on 5/26/20.
 //  Copyright © 2020 Majid Jabrayilov. All rights reserved.
 //
-
 import SwiftUI
+
+final class Store: ObservableObject {
+    @Published var messages: [String] = ["Hello", "World"]
+}
+
+struct MessagesCountView: View {
+    @EnvironmentObject var store: Store
+
+    var body: some View {
+        Text("message count: \(store.messages.count)")
+    }
+}
+
+struct DestinationView: View {
+    @EnvironmentObject var store: Store
+
+    var body: some View {
+        List(store.messages, id: \.self) { message in
+            Text(message)
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            NavigationLink(destination: DestinationView()) {
+                MessagesCountView()
+            }.environmentObject(Store())
+        }
     }
 }
 
